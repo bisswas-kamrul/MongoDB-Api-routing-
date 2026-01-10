d192a72c74fb2b311334f0121808b04a51dc2332
bisswas-kamrul <bisswaskamruldev@gmail.com>
2026-01-10 20:02:11 +0600

    .

--- regex/Emailvirtuals.js
+++ regex/Emailvirtuals.js
@@ -1,5 +1,5 @@
 function Emailvirtuals(email) {
  const Emailregex = /^[a-z0-9][\w\.]{m,n}\@\w+?(\.\w+){1,}$/gi
- Emailregex.test()
+ Emailregex.test() //  Email Address right kina ta justifay korrar jonno
 }
 module.exports = Emailvirtuals
